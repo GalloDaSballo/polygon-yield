523bb2b36ecfbc596c6a3dddf540208cb203b1ee
Entreprenerd <valeranijr@gmail.com>
2021-05-03 21:12:05 +0200

    fix: proper formatting of numbers

--- packages/next/utils/format.ts
+++ packages/next/utils/format.ts
@@ -1,7 +1,12 @@
+const numberWithCommas = (x: string): string =>
+  x.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
 const twoDecimals = (number: number): string => number.toFixed(2);
 
 export const formatPercent = (number: number): string =>
-  `${twoDecimals(Math.round(number * 10000) / 100)}%`;
+  `${numberWithCommas(twoDecimals(Math.round(number * 10000) / 100))}%`;
 
 export const formatStringAmount = (string: string): string =>
-  `${twoDecimals(Math.round(parseFloat(string) * 10000) / 100)}`;
+  `${numberWithCommas(
+    twoDecimals(Math.round(parseFloat(string) * 10000) / 100)
+  )}`;
