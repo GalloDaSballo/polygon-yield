146b6f3de7d887390c447b29a0a199dbe4a1ea1e
Entreprenerd <valeranijr@gmail.com>
2021-05-07 14:37:58 +0200

    feat: subgraph with stats

--- packages/subgraph/generated/schema.ts
+++ packages/subgraph/generated/schema.ts
@@ -69,3 +69,52 @@ export class Account extends Entity {
     this.set("shares", Value.fromBigInt(value));
   }
 }
+
+export class Vault extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Vault entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Vault entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Vault", id.toString(), this);
+  }
+
+  static load(id: string): Vault | null {
+    return store.get("Vault", id) as Vault | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get treasury(): BigInt {
+    let value = this.get("treasury");
+    return value.toBigInt();
+  }
+
+  set treasury(value: BigInt) {
+    this.set("treasury", Value.fromBigInt(value));
+  }
+
+  get totalHarvested(): BigInt {
+    let value = this.get("totalHarvested");
+    return value.toBigInt();
+  }
+
+  set totalHarvested(value: BigInt) {
+    this.set("totalHarvested", Value.fromBigInt(value));
+  }
+}
--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,26 +1,58 @@
-import { DepositCall, WithdrawCall } from '../generated/Myield/Myield'
-import { Account } from '../generated/schema'
+import { BigInt } from '@graphprotocol/graph-ts'
+import { Deposit, Withdraw, Harvest } from '../generated/MyieldWMatic/MyieldWMatic'
+import { Account, Vault } from '../generated/schema'
 
-export function handleDeposit(event: DepositCall): void {
-  const id = event.from.toHex()
+const valuePerShare = (deposited: BigInt, shares: BigInt): BigInt => deposited.div(shares)
+
+export function handleDeposit(event: Deposit): void {
+  const id = event.params.account.toHex()
   let account = Account.load(id)
   if (account == null) {
     account = new Account(id)
+    account.deposited = BigInt.fromI32(0)
+    account.shares = BigInt.fromI32(0)
+    account.earned = BigInt.fromI32(0)
   }
-  account.deposited =  account.deposited.plus(event.inputs.amount)
-  account.shares = event.outputs.value0
-  // This provides us with a ratio
+
+  account.deposited = account.deposited.plus(event.params.amount)
+  account.shares = account.shares.plus(event.params.shares)
 
   account.save()
 }
 
-export function handleWithdrawal(event: WithdrawCall): void {
-  const id = event.from.toHex()
+export function handleWithdrawal(event: Withdraw): void {
+  const id = event.params.account.toHex()
   let account = Account.load(id)
   if (account == null) {
     account = new Account(id)
+    account.deposited = BigInt.fromI32(0)
+    account.shares = BigInt.fromI32(0)
+    account.earned = BigInt.fromI32(0)
   }
 
+  const value = valuePerShare(account.deposited, account.shares)
+  const depositInitialValue = value.times(event.params.shares)
+  const newValue = event.params.value
 
- // do nothing just for now 
+  account.earned = account.earned.plus(newValue.minus(depositInitialValue))
+
+  account.shares = account.shares.minus(event.params.shares)
+  account.deposited = account.deposited.minus(depositInitialValue)
+
+  account.save()
+}
+
+export function handleHarvest(event: Harvest): void {
+  const id = event.address.toHex()
+  let vault = Vault.load(id)
+  if(vault == null){
+    vault = new Vault(id)
+    vault.treasury = BigInt.fromI32(0)
+    vault.totalHarvested = BigInt.fromI32(0)
+  }
+
+  vault.treasury = vault.treasury.plus(event.params.fees)
+  vault.totalHarvested = vault.totalHarvested.plus(event.params.rewardsAmount)
+
+  vault.save()
 }
